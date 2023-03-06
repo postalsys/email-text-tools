209ea508e421e1821866c8750f9b36f74b91270b
Andris Reinman <andris@reinman.eu>
2023-03-06 17:04:16 +0200

    v1.2.1

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const htmlToText = require('./lib/html-to-text');
 const inlineHtml = require('./lib/inline-html');
 const inlineText = require('./lib/inline-text');
 const mimeHtml = require('./lib/mime-html');
+const extractReply = require('./lib/extract-reply');
 
-module.exports = { textToHtml, htmlToText, inlineHtml, inlineText, mimeHtml };
+module.exports = { textToHtml, htmlToText, inlineHtml, inlineText, mimeHtml, extractReply };
