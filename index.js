69a37c6a0896f2308225373718635d69d41e6e39
Andris Reinman <andris@reinman.eu>
2023-05-11 09:45:48 +0300

    v2.0.0

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ const htmlToText = require('./lib/html-to-text');
 const inlineHtml = require('./lib/inline-html');
 const inlineText = require('./lib/inline-text');
 const mimeHtml = require('./lib/mime-html');
-const generateSummary = require('./lib/generate-summary');
-const riskAnalysis = require('./lib/risk-analysis');
 
 module.exports = {
     textToHtml,
     htmlToText,
     inlineHtml,
     inlineText,
-    mimeHtml,
-    generateSummary,
-    riskAnalysis
+    mimeHtml
 };
