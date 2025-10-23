a2969e9399ed6c9d15a85d7d8c11342bc5bf00ce
Andris Reinman <andris@reinman.eu>
2025-10-23 11:09:19 +0300

    fix: Downgraded jsdom to 27.0.0

--- .ncurc.js
+++ .ncurc.js
@@ -2,6 +2,7 @@ module.exports = {
     upgrade: true,
     reject: [
         // Block package upgrades that moved to ESM
-        'nanoid'
+        'nanoid',
+        'jsdom' // parse5 dependency in 27.0.1 is ESM only
     ]
 };
